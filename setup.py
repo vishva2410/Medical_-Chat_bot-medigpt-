27a70ab1261def4254e8b1faabe453fd2047439b
vishva2410 <vishvateja10@gmail.com>
2025-08-21 23:56:39 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='end_to_end_medical_chatbot',
+    version='0.0.1',
+    author='vishva teja',
+    author_email='vishvateja10@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
